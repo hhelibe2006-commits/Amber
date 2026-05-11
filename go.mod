45610862784b43816e06b5b8d1b03d6b037ed4ad
hhelibe2006-commits <hhelibe2006@gmail.com>
2026-05-11 21:15:57 +0800

    将技术栈更改为go

---
+++ go.mod
@@ -0,0 +1,5 @@
+module os
+
+go 1.26.3
+
+require fyne.io/fyne/v2 v2.7.3 // indirect
--- src/utils/centered_ui.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include<QScreen>
-
-template<typename T>
-void centered_ui(T *windows) {
-    QRect screen = QGuiApplication::primaryScreen()->availableGeometry();
-    auto size = windows -> geometry();
-    windows -> move((screen.width() - size.width()) / 2, (screen.height() - size.height()) / 2);
-}
